314c1eede7f36673abd74636aa87117dab7dfbbe
Rachidoulgour <olgor@hotmail.com>
2024-04-14 13:38:56 +0200

    getJob and getCompany added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import express from 'express';
+import { readFile } from 'node:fs/promises';
 import { authMiddleware, handleLogin } from './auth.js';
+import { resolvers } from './resolvers';
 
 const PORT = 9000;
 
@@ -10,10 +12,14 @@ const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
-const apolloServer = new ApolloServer({});
+
+const typeDefs = await readFile('./schema.graphql', 'utf8');
+
+const apolloServer = new ApolloServer({typeDefs, resolvers});
 await apolloServer.start();
 app.use('/graphql', apolloMiddleware(apolloServer));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQl: http://localhost:${PORT}/graphql`);
 });
