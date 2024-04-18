907eb0d02ada4301f20024521492f56ed4c67e79
Rachidoulgour <olgor@hotmail.com>
2024-04-18 13:33:05 +0200

    get company by id added to server and client

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import { getCompany } from '../lib/graphql/queries';
+//import { companies } from '../lib/fake-data';
 
 function CompanyPage() {
   const { companyId } = useParams();
+  const [company, setCompany] = useState();
+  useEffect(() => {
+    getCompany(companyId).then(setCompany);
+  })
+  if(!company) {
+    return <div>Loading...</div>
+  }
 
-  const company = companies.find((company) => company.id === companyId);
+  //const company = companies.find((company) => company.id === companyId);
   return (
     <div>
       <h1 className="title">
