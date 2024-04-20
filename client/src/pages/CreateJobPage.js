da022d6bc869c894d626f817e6a150c7a0ecc8cb
Rachidoulgour <olgor@hotmail.com>
2024-04-20 21:07:37 +0200

    createJob added to client

--- client/src/pages/CreateJobPage.js
+++ client/src/pages/CreateJobPage.js
@@ -1,12 +1,17 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { createJob } from '../lib/graphql/queries';
 
 function CreateJobPage() {
+  const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    const job = await createJob({ title, description });
     console.log('should post a new job:', { title, description });
+    navigate(`/jobs/${job.id}`);
   };
 
   return (
